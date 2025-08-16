d4d93b2a67a8e57fe53d329e4a86f073a704e5e0
GlobalSaturation <87565097+GlobalSaturation@users.noreply.github.com>
2025-08-16 16:35:33 -0700

    Add "x" button functionality & improve list input
    The input box has been improved a little to display
    an error message when nothing is input. Additionally,
    the enter key can now be used to add a list item
    instead of the add button

--- javascript.js
+++ javascript.js
@@ -4,10 +4,11 @@ const list = document.querySelector("ul");
 
 function createIcon(iconId) {
 	//use an already defined symbol and package it into an svg element
-	const useTag = document.createElementNS("http://www.w3.org/2000/svg", "use");
+	const svgURI = "http://www.w3.org/2000/svg";
+	const useTag = document.createElementNS(svgURI, "use");
 	useTag.setAttribute("href", `#${iconId}`);
 
-	const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+	const svg = document.createElementNS(svgURI, "svg");
 	svg.classList.add("icon");
 	svg.appendChild(useTag);
 
@@ -22,16 +23,34 @@ function createListItem() {
 	const deleteButton = document.createElement("button");
 	deleteButton.appendChild(createIcon("deleteId"));
 	deleteButton.setAttribute("aria-label", "Delete item");//alt text
-	//TODO: make delete button functional
+	//make delete button functional
+	deleteButton.addEventListener("click", () => {
+		list.removeChild(newItem);
+	});
 
 	newItem.appendChild(deleteButton);
 	list.appendChild(newItem);
 }
 
-//create list element
-addBtn.addEventListener("click", () => {
-	//TODO: display error message when no input is given
+function attemptListItem() {
+	const errorMsg = document.querySelector(".error-message");
+	// itemInput.focus();
+
+	//TODO: add a character limit
 	if (itemInput.value != "") {
+		errorMsg.style.display = "none";
 		createListItem();
+	} else {
+		//show error message
+		errorMsg.style.display = "block";
+	}
+}
+
+//create list element either by clicking the button or pressing enter
+addBtn.addEventListener("click", attemptListItem);
+
+itemInput.addEventListener("keyup", (event) => {
+	if (event.key === "Enter") {
+		attemptListItem();
 	}
 });
